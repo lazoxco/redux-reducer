10de2c71ad0032bc6bde3ec02c7d91fc1a867cfb
github username <github email address>
2021-06-07 23:06:03 -0400

    Working through lab.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,21 @@
 export function managePresents(state, action){
+
+  state = {
+    numberOfPresents: 0
+  }
+  
+  action = {
+    type: 'INCREASE'
+  }
+
+    switch (action.type) {
+      case 'INCREASE':
+        return {
+          ...state, 
+          numberOfPresents: state.numberOfPresents + 1
+        }
+      default:
+        return state
+    }
+
 }
